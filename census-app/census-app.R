f451a438b5f97e7f72301f722f0e8739c3faee01
Tony Isebe <48394241+Tisebe@users.noreply.github.com>
2020-03-26 10:00:48 +0300

    Creating a reactive app.

---
+++ census-app/census-app.R
@@ -0,0 +1,49 @@
+library(shiny)
+
+#Define UI
+
+ui <- fluidPage(
+  titlePanel('censusVis'),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      helpText('Create demographic maps with 
+               information from the 2010 US Census.'),
+      
+      selectInput('var',
+                  label = 'Choose a variable to display.',
+                  choices = list('Percent White',
+                                 'Percent Black',
+                                 'Percent Hispanic',
+                                 'Percent Asian'),
+                  selected = 'Percent White'),
+      sliderInput('range',
+                  label = 'Range of Interest:',
+                  min = 0, max = 100, value = c(0,100))
+      
+    ),
+    mainPanel(
+      textOutput('selected_var'),
+      textOutput('min_max')
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$selected_var <- renderText({
+    paste('You have selected', input$var)
+  }) 
+    output$min_max <- renderText({
+      paste('You have chosen a range that goes from',
+            input$range[1], 'to', input$range[2])
+    
+  })
+  
+}
+
+
+#Define App
+
+shinyApp(ui=ui, server = server)
